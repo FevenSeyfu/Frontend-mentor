69d7a792cd3c17a52e6fb50c6e9df83c9a7dcce6
FevenSeyfu <fevenseyfu00@gmail.com>
2024-05-03 23:57:52 +0300

    Add style for desktop

--- base-apparel-coming-soon/script.js
+++ base-apparel-coming-soon/script.js
@@ -1,11 +1,11 @@
 const heroImg  = document.getElementById('hero-image');
 
-window.addEventListener('load',()=>{
-    console.log('inner'+ window.innerWidth)
-    console.log(window.innerWidth)
-    if (window.innerWidth <= 375) {
-        heroImg.src = './images/hero-mobile.jpg'
+const HandleHeroImg = () =>{
+    if (window.innerWidth >= 768) {
+        heroImg.src = './images/hero-desktop.jpg'
     }
-})
+}
+window.addEventListener('load',HandleHeroImg)
+window.addEventListener('resize',HandleHeroImg)
 
 
