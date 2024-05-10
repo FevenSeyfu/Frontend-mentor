e6dd548776bdfdc9cd041be904a5bfa26f05a011
FevenSeyfu <fevenseyfu00@gmail.com>
2024-05-10 19:48:40 +0300

    Complete form validation and submission

--- base-apparel-coming-soon/script.js
+++ base-apparel-coming-soon/script.js
@@ -1,4 +1,9 @@
 const heroImg  = document.getElementById('hero-image');
+const emailInput = document.getElementById('email-input');
+const message = document.getElementById('message');
+const submitBtn = document.getElementById('submit-btn')
+const form  = document.querySelector('form');
+let email = ''
 
 const HandleHeroImg = () =>{
     if (window.innerWidth >= 768) {
@@ -9,3 +14,37 @@ window.addEventListener('load',HandleHeroImg)
 window.addEventListener('resize',HandleHeroImg)
 
 
+// check if email is valid
+const isValidEmail = (email) => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+}
+const handleChange = (e) => {
+    if(emailInput.value.trim() == '' || !isValidEmail(emailInput.value)){
+        emailInput.classList.add('error');
+        message.textContent = 'Please provide a valid email';
+        message.style.color = 'var(--soft-red)';
+        message.classList.remove('hidden');
+        submitBtn.disabled = true;
+    } else {
+        emailInput.classList.remove('error');
+        email= emailInput.value;
+        submitBtn.disabled = false;
+        message.classList.add('hidden');
+    }
+}
+const handleFormSubmission = (e) => {
+  e.preventDefault();
+  if(email.trim !==''){
+    message.textContent = 'Email Successfully Added!';
+    message.style.color = 'green';
+    message.classList.remove('hidden');
+    setTimeout(() => {
+        message.classList.add('hidden');
+    }, 10000);
+     emailInput.value = '';
+  }
+}
+
+emailInput.addEventListener('input', (e) => handleChange(e));
+form.addEventListener('submit',(e)=>handleFormSubmission(e));
